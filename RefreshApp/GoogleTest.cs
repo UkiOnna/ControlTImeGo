4bc01310a2faa1c24db9c74ea16a87acf76360f9
UkiOnna <sikirovtemirlan@mail.ru>
2018-05-11 18:23:48 +0600

    Fan

---
+++ RefreshApp/GoogleTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis;
+using Google.Apis.Download;
+using System.Threading;
+using Google.Apis.Services;
+using System.IO;
+using Google.Apis.Util.Store;
+using Google.Apis.Drive.v3;
+using Google.Apis.Auth.OAuth2;
+
+namespace RefreshApp
+{
+   public class GoogleTest
+    {
+        public static DriveService AuthenticateOauth(string clientSecretJson, string userName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userName))
+                    throw new ArgumentNullException("userName");
+                if (string.IsNullOrEmpty(clientSecretJson))
+                    throw new ArgumentNullException("clientSecretJson");
+                if (!File.Exists(clientSecretJson))
+                    throw new Exception("clientSecretJson file does not exist.");
+
+                // These are the scopes of permissions you need. It is best to request only what you need and not all of them
+                string[] scopes = new string[] { DriveService.Scope.DriveReadonly };         	//View the files in your Google Drive                                                 
+                UserCredential credential;
+                using (var stream = new FileStream(clientSecretJson, FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                    credPath = Path.Combine(credPath, ".credentials/", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+
+                    // Requesting Authentication or loading previously stored authentication for userName
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets,
+                                                                             scopes,
+                                                                             userName,
+                                                                             CancellationToken.None,
+                                                                             new FileDataStore(credPath, true)).Result;
+                }
+
+                // Create Drive API service.
+                return new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Drive Oauth2 Authentication Sample"
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Create Oauth2 account DriveService failed" + ex.Message);
+                throw new Exception("CreateServiceAccountDriveFailed", ex);
+            }
+        }
+
+
+        private static void DownloadFile(Google.Apis.Drive.v3.DriveService service, Google.Apis.Drive.v3.Data.File file, string saveTo)
+        {
+
+            var request = service.Files.Get(file.Id);
+            var stream = new System.IO.MemoryStream();
+
+            // Add a handler which will be notified on progress changes.
+            // It will notify on each chunk download and when the
+            // download is completed or failed.
+            request.MediaDownloader.ProgressChanged += (Google.Apis.Download.IDownloadProgress progress) =>
+            {
+                switch (progress.Status)
+                {
+                    case Google.Apis.Download.DownloadStatus.Downloading:
+                        {
+                            Console.WriteLine(progress.BytesDownloaded);
+                            break;
+                        }
+                    case Google.Apis.Download.DownloadStatus.Completed:
+                        {
+                            Console.WriteLine("Download complete.");
+                            SaveStream(stream, saveTo);
+                            break;
+                        }
+                    case Google.Apis.Download.DownloadStatus.Failed:
+                        {
+                            Console.WriteLine("Download failed.");
+                            break;
+                        }
+                }
+            };
+            request.Download(stream);
+
+        }
+
+
+        private static void SaveStream(System.IO.MemoryStream stream, string saveTo)
+        {
+            using (System.IO.FileStream file = new System.IO.FileStream(saveTo, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+            {
+                stream.WriteTo(file);
+            }
+        }
+
+    }
+}
---
+++ RefreshApp/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RefreshApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Timer timer;
+        private Uri iconUri;
+        Application cur;
+        private object lockObject=new object();
+        public MainWindow()
+        {
+            InitializeComponent();
+            iconUri = new Uri(@"C:\Users\СикировТ\Documents\Visual Studio 2017\Projects\RefreshApp\RefreshApp\Icons\icon.jpg", UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
+            cur = Application.Current;
+            timer = new Timer(CheckFiles, cur, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(15)); 
+        }
+
+
+
+        public void CheckFiles(object obj)
+        {
+
+            Application mes = (Application)obj;
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                (ThreadStart)delegate ()
+                {
+
+                 //   try
+                   // {
+                        // Only get files that begin with the letter "c."
+                        string[] icons = Directory.GetFiles(@"C:\Users\СикировТ\Documents\files", "icon*");//хранятся пути 
+                        string[] backgronds = Directory.GetFiles(@"C:\Users\СикировТ\Documents\files", "Background*");
+                    
+                        if (icons.Length > 0 && backgronds.Length > 0)
+                        {
+                        if (FileCompare(icons[0], @"C:\Users\СикировТ\Documents\Visual Studio 2017\Projects\RefreshApp\RefreshApp\Icons\icon.jpg") && FileCompare(backgronds[0], @"C:\Users\СикировТ\Documents\Visual Studio 2017\Projects\RefreshApp\RefreshApp\Backrounds\Backround.jpg"))
+                        {
+
+
+                            MessageBoxResult res = MessageBox.Show("Обнаружены обновления хотите загрузить?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                            if (MessageBoxResult.No == res)
+                            {
+                                //do no stuff
+                            }
+                            else
+                            {
+
+                                FileChange(icons[0], backgronds[0]);
+                                timer.Dispose();
+                                System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+                                mes.Shutdown();
+
+
+                                //do yes stuff
+                            }
+                        }
+                        }
+
+                  //  }
+                  //  catch (Exception e)
+                    //{
+                     //   MessageBox.Show("The process failed: {0}", e.ToString());
+                    //}
+                }
+                );
+        }
+
+        public void FileChange(string iconPathh,string backPathh)
+        {
+            string iconPath = @"C:\Users\СикировТ\Documents\Visual Studio 2017\Projects\RefreshApp\RefreshApp\Icons\icon.jpg";
+            string backPath = @"C:\Users\СикировТ\Documents\Visual Studio 2017\Projects\RefreshApp\RefreshApp\Backrounds\Backround.jpg";
+            FileInfo icoInf = new FileInfo(iconPath);
+            FileInfo backInf = new FileInfo(backPath);
+            if (icoInf.Exists && backInf.Exists)
+            {
+                icoInf.Delete();
+                backInf.Delete();
+                // альтернатива с помощью класса File
+                // File.Delete(path);
+            }
+
+            string newIconPath = @"C:\Users\СикировТ\Documents\Visual Studio 2017\Projects\RefreshApp\RefreshApp\Icons\icon.jpg";
+
+            string newBackPath = @"C:\Users\СикировТ\Documents\Visual Studio 2017\Projects\RefreshApp\RefreshApp\Backrounds\Backround.jpg";
+
+            FileInfo icoFileInf = new FileInfo(iconPath);
+            FileInfo backFileInf = new FileInfo(backPath);
+            if (icoFileInf.Exists && backFileInf.Exists) ;
+            {
+                icoFileInf.MoveTo(newIconPath);
+                backFileInf.MoveTo(newBackPath);
+                // альтернатива с помощью класса File
+                // File.Move(path, newPath);
+            }
+
+        }
+
+        private bool FileCompare(string file1, string file2)
+        {
+            lock (lockObject)
+            {
+                int file1byte;
+                int file2byte;
+                FileStream fs1;
+                FileStream fs2;
+
+                if (file1 == file2)
+                {
+                    return true;
+                }
+
+                fs1 = new FileStream(file1, FileMode.Open);
+                fs2 = new FileStream(file2, FileMode.Open);
+
+                if (fs1.Length != fs2.Length)
+                {
+                    fs1.Close();
+                    fs2.Close();
+
+                    return false;
+                }
+
+                do
+                {
+                    // Read one byte from each file.
+                    file1byte = fs1.ReadByte();
+                    file2byte = fs2.ReadByte();
+                }
+                while ((file1byte == file2byte) && (file1byte != -1));
+
+                fs1.Close();
+                fs2.Close();
+
+                return ((file1byte - file2byte) == 0);
+            }
+        }
+
+
+    }
+}
+
+
